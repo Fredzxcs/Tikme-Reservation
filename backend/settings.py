05b9868b94210a4a846845c2f1516070cd9e02d9
fredzxc <johnfredericktsabondo@iskolarngbayan.pup.edu.ph>
2025-02-15 08:22:37 +0800

    ready for render6

--- backend/settings.py
+++ backend/settings.py
@@ -42,7 +42,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8006",  # Frontend URL
     "http://localhost:8006",
-    "https://capstone-paymentgateway.onrender.com/",
+    "https://capstone-paymentgateway.onrender.com",
 ]
 
 ROOT_URLCONF = 'backend.urls'
